cb86e873f0f3bfc69facba07c0a0b5d67386f5f3
kofi amoussou <karsoft92@gmail.com>
2016-11-20 20:02:00 +0000

    new styles

--- pricing.php
+++ pricing.php
@@ -2,65 +2,73 @@
 
 <section class="row">
   <section class="container price-model">
-    <section class="col-md-4">
-      <section class="pricing-header">
-        <h3>Basic</h3>
-      </section>
-      <section class="pricing">
-        
-      </section>
-
-      <section class="pricing-features">
-        <ul>
-          <li>School & Study Program</li>
-          <li>Accomodation</li>
-          <li>Flight</li>
-          <li>Support</li>
-           <li><em>This service does NOT GUARANTEE visa</em></li>
-        </ul>
-      </section>
+    <section class="col-md-4 ">
+    <section class="thumbnail">  <section class="pricing-header  page-header">
+            <h3>Basic</h3>
+          </section>
+          <section class="pricing">
+                 <h3 class="text-center"> <small>C</small> 100 <small>.00</small> </h3>
+               <h5 class="text-center">/user</h5>
+          </section>
+    
+          <section class="pricing-features">
+            <ul class="featured-list">
+              <li class="list-yes">School & Study Program</li>
+              <li class="list-no">Accomodation</li>
+              <li class="list-no">Flight</li>
+              <li class="list-no">Support</li>
+               <li><em>This service does NOT GUARANTEE visa</em></li>
+            </ul>
+          </section></section>
     </section>
 
 
-    <section class="col-md-4">
-        <section class="pricing-header">
-        <h3>Deluxe</h3>
-      </section>
-      <section class="pricing">
-        
-      </section>
+    <section class="col-md-4 ">
+       <section class="thumbnail"> 
+       <section class="pricing-header  page-header">
+               <h3>Deluxe</h3>
+             </section>
+             <section class="pricing">
+               <h3 class="text-center"> <small>C</small> 250 <small>.00</small> </h3>
+               <h5 class="text-center">/user</h5>
 
-      <section class="pricing-features">
-        <ul>
-          <li>School & Study Program</li>
-          <li>Accomodation</li>
-          <li>Flight</li>
-          <li>Support</li>
-          <li><em>This service does NOT GUARANTEE visa</em></li>
-        </ul>
-      </section>
+
+             </section>
+       
+             <section class="pricing-features">
+               <ul class="featured-list">
+                 <li  class="list-yes">School & Study Program</li>
+                 <li class="list-yes">Accomodation</li>
+                 <li class="list-no">Flight</li>
+                 <li  class="list-no">Support</li>
+                 <li><em>This service does NOT GUARANTEE visa</em></li>
+               </ul>
+             </section>
+        </section>
     </section>
 
 
 
-    <section class="col-md-4">
+    <section class="col-md-4 ">
+      <section class="thumbnail">
+              <section class="phd-orange page-header">
+              <h3>Complete</h3>
+            </section>
+            <section class="pricing">
+                 <h3 class="text-center"> <small>C</small> 500 <small>.00</small> </h3>
+               <h5 class="text-center">/user</h5>
+              
+            </section>
       
-        <section class="pricing-header">
-        <h3>Complete</h3>
-      </section>
-      <section class="pricing">
-        
-      </section>
-
-      <section class="pricing-features">
-        <ul>
-          <li>School & Study Program</li>
-          <li>Accomodation</li>
-          <li>Flight</li>
-          <li>Support</li>
-          <li><em>This service does NOT GUARANTEE visa</em></li>
-        </ul>
-      </section>
+            <section class="pricing-features">
+              <ul class="featured-list">
+                <li class="list-yes">School & Study Program</li>
+                <li  class="list-yes">Accomodation</li>
+                <li  class="list-yes">Flight</li>
+                <li  class="list-yes">Support</li>
+                <li><em>This service does NOT GUARANTEE visa</em></li>
+              </ul>
+            </section></section>
     </section>
   </section>
 
